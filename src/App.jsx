3b991e93dcb9758726352245ad330e4ecbc5e6c3
Shahin Hossain <shahiniubat99@gmail.com>
2023-08-07 22:28:42 +0600

    Final-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
 import Blog from './components/Blog/Blog'
-import QuestionAnswe from './components/QuestionAnswer/QuestionAnswe'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +11,6 @@ function App() {
     <div className="App">
     <Header></Header>
     <Blog></Blog>
-    <QuestionAnswe></QuestionAnswe>
     </div>
   )
 }
--- src/components/SingleBlog/SingleBlog.jsx
+++ src/components/SingleBlog/SingleBlog.jsx
@@ -1,48 +1,62 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBookBookmark } from '@fortawesome/free-solid-svg-icons'
-import './SingleBlog.css'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookBookmark } from "@fortawesome/free-solid-svg-icons";
+import "./SingleBlog.css";
 
-const SingleBlog = ({ blog,handleBookmarkedBlog,handleTimeRead}) => {
-    const { id, author_name, blog_title, authorImg, coverImg, read_time, published_date } = blog;
+const SingleBlog = ({ blog, handleBookmarkedBlog, handleTimeRead }) => {
+  const {
+    id,
+    author_name,
+    blog_title,
+    authorImg,
+    coverImg,
+    read_time,
+    published_date,
+  } = blog;
 
-
-    return (
+  return (
+    <div>
+      <div className="single-card p-5">
         <div>
-            <div className='single-card p-5'>
-                <div>
-                    <img className='rounded-3xl lg: h-96  w-full' src={coverImg} alt="" />
-                </div>
-                <div className='flex justify-between  items-center mt-5'>
-                    <div className='flex items-center  '>
-                        <div>
-                            <img className='w-10 rounded-full ' src={authorImg} alt="" />
-                        </div>
-                        <div className='ps-5'>
-                            <h5 className='font-bold text-lg'>{author_name}</h5>
-                            <p>{published_date}</p>
-                        </div>
-                    </div>
-
-                    <div className='flex items-center gap-2'>
-                        <p className='cursor-pointer'>{read_time} min read</p>
-                        <p onClick={()=>handleBookmarkedBlog(blog_title)}><FontAwesomeIcon icon={faBookBookmark} /></p>
-                    </div>
-                   
-
-                </div>
-
-                <div className='my-3'>
-                    <h2 className='lg:text-4xl font-bold my-3 '>{blog_title}</h2>
-                </div>
-                <div className='text-blue-700 underline '>
-                <p className='cursor-pointer' onClick={() => handleTimeRead(read_time)}>Mark as read</p>
-                </div>
-
-            </div>
-            <hr className='custom-divider' />
+          <img className="rounded-3xl lg: h-96  w-full" src={coverImg} alt="" />
         </div>
-    );
+        <div className="flex justify-between  items-center mt-5">
+          <div className="flex items-center  ">
+            <div>
+              <img className="w-10 rounded-full " src={authorImg} alt="" />
+            </div>
+            <div className="ps-5">
+              <h5 className="font-bold text-lg">{author_name}</h5>
+              <p>{published_date}</p>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <p>{read_time} min read</p>
+            <p
+              className="cursor-pointer"
+              onClick={() => handleBookmarkedBlog(blog_title)}
+            >
+              <FontAwesomeIcon icon={faBookBookmark} />
+            </p>
+          </div>
+        </div>
+
+        <div className="my-3">
+          <h2 className="lg:text-4xl font-bold my-3 ">{blog_title}</h2>
+        </div>
+        <div className="text-blue-700 underline ">
+          <p
+            className="cursor-pointer"
+            onClick={() => handleTimeRead(read_time)}
+          >
+            Mark as read
+          </p>
+        </div>
+      </div>
+      <hr className="custom-divider" />
+    </div>
+  );
 };
 
 export default SingleBlog;
